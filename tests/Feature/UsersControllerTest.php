a842ef3a7a790ae71a1b007f02ff74aac21edc31
Jakhongir91 <jakhongir.nishanbaev@gmail.com>
2024-12-30 15:15:57 +0500

    Start TDD, add tests

--- tests/Feature/UsersControllerTest.php
+++ tests/Feature/UsersControllerTest.php
@@ -4,19 +4,19 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 it("has authorization check", function () {
-    $response = $this->get('/api/users');
+    $response = $this->get('/api/clients');
 
     $response->assertStatus(403);
 });
 
 it("returns success http status code", function () {
-    $response = $this->get('/api/users');
+    $response = $this->get('/api/clients');
 
     $response->assertStatus(200);
 });
 
-it("shows list of users without pagination", function () {
-    $response = $this->get('/'); // '/api/users'
+it("shows list of clients without pagination", function () {
+    $response = $this->get('/api/clients'); 
 
     $response->assertJson('
         {
