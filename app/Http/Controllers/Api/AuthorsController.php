de5080a7a55caf803af25afe2f29f1875d6131b6
Jakhongir91 <jakhongir.nishanbaev@gmail.com>
2024-12-30 16:33:00 +0500

    Pass 3rd test

--- app/Http/Controllers/Api/AuthorsController.php
+++ app/Http/Controllers/Api/AuthorsController.php
@@ -10,6 +10,23 @@ class AuthorsController extends Controller
 {
     public function index()
     {
+        $authors = Author::get();
+        $result = [];
+
+        foreach ($authors as $author) {
+            $result[] = [
+                "id" => $author->id,
+                "name" => $author->name,
+            ];
+        }
+
+        return [
+            "data" => [
+                "returnType" => "collection",
+                "paginate" => false,
+                "result" => $result,
+            ]
+        ];
         
         return response()->json(["data" => "success"]);
     }
